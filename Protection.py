f1df22707ae7006c15cf07c5efacc8e5d961f8dd
wnaveed12 <129912489+wnaveed12@users.noreply.github.com>
2023-04-25 14:35:09 -0500

    Update Protection.py

--- Protection.py
+++ Protection.py
@@ -1,36 +1,27 @@
 import subprocess
-from scapy.layers.eap import EAPOL
+from scapy.layers.dot11 import Dot11, Dot11Deauth
 from scapy.sendrecv import sniff
 from threading import Thread
 import tkinter as tk
 
 def monitor_traffic():
     # Set up a Scapy sniffing function to capture wireless traffic
-    sniff(prn=process_packet, filter="wlan type data")
+    sniff(prn=process_packet, filter="wlan type management subtype deauth")
 
 def process_packet(packet):
-    # Check if the packet is a handshake packet
-    if packet.haslayer(EAPOL):
-        # Check if the packet is a 4-way handshake packet
-        if packet[EAPOL].type == 3:
-            # Get the key information from the packet
-            key_info = packet[EAPOL].key_info
+    # Display a message when deauthentication attack is detected
+    label.config(text="Deauthentication attack detected!")
+    label.update()
 
-            # Check if the key information indicates a KRACK attack
-            if key_info & 0x1 == 0 and key_info & 0x2 == 0 and key_info & 0x4 == 0 and key_info & 0x40 == 0:
-                # Display a message when KRACK attack is detected
-                label.config(text="KRACK attack detected!")
-                label.update()
+    # Perform the protection code to mitigate the attack
+    # ...
 
-                # Perform the protection code to mitigate the attack
-                # ...
+    # Display a message when protection code is implemented
+    label.config(text="Protection code implemented!")
+    label.update()
 
-                # Display a message when protection code is implemented
-                label.config(text="Protection code implemented!")
-                label.update()
-
-                # Add the bad packet details to the listbox
-                listbox.insert(tk.END, f"Source: {packet.addr2}    Destination: {packet.addr1}    Type: {packet.type}")
+    # Add the bad packet details to the listbox
+    listbox.insert(tk.END, f"Source: {packet.addr2}    Destination: {packet.addr1}    Type: {packet.type}")
 
 def check_updates():
     # Run the wmic command to check for security updates
@@ -40,7 +31,7 @@ def check_updates():
 
     # Check the output for security updates
     if b'Security Update' in output:
-        label.config(text="Security updates are installed. Click the button to start monitoring for KRACK attacks.")
+        label.config(text="Security updates are installed. Click the button to start monitoring for deauthentication attacks.")
         button.config(state=tk.NORMAL)
     else:
         label.config(text="Security updates are not installed. Please install updates and try again.")
@@ -52,7 +43,7 @@ def start_monitoring():
 
 # Set up the GUI
 root = tk.Tk()
-root.title("KRACK Protection")
+root.title("Deauthentication Attack Protection")
 root.geometry("400x300")
 
 # Add a label to display status messages
