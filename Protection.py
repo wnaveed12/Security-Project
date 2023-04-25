66a20d3db2c36bb9a0cb677475119ccd5f795bbf
wnaveed12 <129912489+wnaveed12@users.noreply.github.com>
2023-04-25 14:19:58 -0500

    Update Protection.py

--- Protection.py
+++ Protection.py
@@ -1,3 +1,4 @@
+import subprocess
 from scapy.layers.eap import EAPOL
 from scapy.sendrecv import sniff
 from threading import Thread
@@ -31,6 +32,20 @@ def process_packet(packet):
                 # Add the bad packet details to the listbox
                 listbox.insert(tk.END, f"Source: {packet.addr2}    Destination: {packet.addr1}    Type: {packet.type}")
 
+def check_updates():
+    # Run the wmic command to check for security updates
+    cmd = 'wmic qfe get Caption,Description,HotFixID,InstalledOn'
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    output = proc.communicate()[0]
+
+    # Check the output for security updates
+    if b'Security Update' in output:
+        label.config(text="Security updates are installed. Click the button to start monitoring for KRACK attacks.")
+        button.config(state=tk.NORMAL)
+    else:
+        label.config(text="Security updates are not installed. Please install updates and try again.")
+        button.config(state=tk.DISABLED)
+
 def start_monitoring():
     # Start the traffic monitoring thread
     Thread(target=monitor_traffic).start()
@@ -41,11 +56,14 @@ root.title("KRACK Protection")
 root.geometry("400x300")
 
 # Add a label to display status messages
-label = tk.Label(root, text="Click the button to start monitoring for KRACK attacks.")
+label = tk.Label(root, text="Checking for security updates...")
 label.pack()
 
+# Check for security updates before enabling the button
+Thread(target=check_updates).start()
+
 # Add a button to start monitoring
-button = tk.Button(root, text="Start monitoring", command=start_monitoring)
+button = tk.Button(root, text="Start monitoring", command=start_monitoring, state=tk.DISABLED)
 button.pack()
 
 # Add a listbox to display the details of bad packets
